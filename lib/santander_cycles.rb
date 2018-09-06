35713448955bad51f359e31b0a9d65d5bf24acbe
Alex Kharouk <akharouk@gmail.com>
2018-09-06 15:13:06 +0100

    refactoring and add tests

--- lib/santander_cycles.rb
+++ lib/santander_cycles.rb
@@ -1,15 +1,38 @@
 puts "File is Loaded..."
 
 class DockingStation
-    attr_reader :bike
+    attr_reader :list_of_bikes, :DEFAULT_CAPACITY
+
+    def initialize
+        @list_of_bikes = []
+        @DEFAULT_CAPACITY = 20
+    end
+    
 
     def release_bike
-        Bike.new
+
+        raise "No bikes!!!!!" if empty?
+        removed_bike = @list_of_bikes.sample
+        @list_of_bikes.delete(removed_bike)
+        removed_bike
+
     end
 
     def dock(bike)
-       @bike = bike
+       raise "Too many bikes!!!!!" if full?
+       @list_of_bikes << bike
     end
+    
+
+    private 
+    def full?
+        @list_of_bikes.size >= @DEFAULT_CAPACITY
+    end
+
+    def empty?
+        @list_of_bikes.empty?
+    end
+
 end
 
 class Bike
@@ -19,6 +42,6 @@ class Bike
 end
 
 
-# docking_station = DockingStation.new
+docking_station = DockingStation.new
 
 # puts docking_station.respond_to? :release_bike
--- spec/santander_cycles_spec.rb
+++ spec/santander_cycles_spec.rb
@@ -1,25 +1,35 @@
 require 'santander_cycles'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
+    # it { is_expected.to respond_to :release_bike }
 
-    it 'expects release_bike to instantiate new Bike class' do
-        bike = Bike.new
-        expect(bike.working?).to eq true
-    end
+    # it 'expects release_bike to instantiate new Bike class' do
+    #     bike = Bike.new
+    #     expect(bike.working?).to eq true
+    # end
 
     
-    it { is_expected.to respond_to(:dock).with(1).argument }
+    # it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it 'expects user to check if bikes are docked' do
-        bike = Bike.new
-        expect(subject.dock(bike)).to eq bike
+    # it 'expects user to check if bikes are docked' do
+    #     bike = Bike.new
+    #     expect(subject.dock(bike)).to eq bike
+    # end
+
+    # it 'returns us all of the docked bikes at station' do
+    #     bike = Bike.new
+    #     subject.dock(bike)
+    #     expect(subject.bike).to eq bike
+    # end
+
+    it 'raises an error when user removes bike when there is no bikes' do
+        # curly braces kinda yields out/doesn't run right away until runs all of line
+        expect { subject.release_bike }.to raise_error "No bikes!!!!!"
     end
 
-    it 'returns us all of the docked bikes at station' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
-    end
-
+    it "only allows ten bikes to be docked" do
+        #subject.list_of_bikes > 10 and try to dock bike, return error
+        subject.DEFAULT_CAPACITY.times { |n| subject.dock(Bike.new) }
+        expect { subject.dock(Bike.new) }.to raise_error "Too many bikes!!!!!"
+    end 
 end
